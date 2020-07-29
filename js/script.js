8fcabd1f43323f1e88ad1d4b768e4ba71c56ee81
mebadong-kor <c.mebadong@gmail.com>
2020-07-29 23:03:16 +0900

    favicon 추가 및 font 에러 수정

--- js/script.js
+++ js/script.js
@@ -6,3 +6,8 @@ document.addEventListener('DOMContentLoaded', function() {
     let elems = document.querySelectorAll('.timepicker');
     let instances = M.Timepicker.init(elems, timePickerOpt);
 });
+
+let calculateTime = function() {
+    let wakeTime = document.querySelector('.timepicker')
+    console.log()
+}
