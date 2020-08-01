1342379312363d5670315d082942007007af0c5c
mebadong-kor <c.mebadong@gmail.com>
2020-08-02 01:02:07 +0900

    기능 완성

--- js/script.js
+++ js/script.js
@@ -1,13 +1,39 @@
+let modalInstances;
+
 let timePickerOpt = {
     twelveHour: false
 };
 
-document.addEventListener('DOMContentLoaded', function() {
-    let elems = document.querySelectorAll('.timepicker');
-    let instances = M.Timepicker.init(elems, timePickerOpt);
-});
-
 let calculateTime = function() {
-    let wakeTime = document.querySelector('.timepicker')
-    console.log()
-}
+    let wakeTime = document.querySelector('.timepicker').value;
+    if (wakeTime == "") {
+        M.toast({
+            html: '일어날 시간을 입력하세요!'
+        })
+
+        return;
+    }
+    let recommandTimeSpan = document.querySelectorAll('.recommand-text');
+    let recommandCoffeeSpan = document.querySelectorAll('.coffee-time');
+    let wakeTimeDate = new Date(Date.prototype.setHours.apply(new Date(), wakeTime.split(':')));
+    let recommendTime = [];
+    let recommendCoffeeTime = new Date(wakeTimeDate - (-9000000)).toTimeString().slice(0, 5);
+
+    recommandCoffeeSpan[0].innerHTML = recommendCoffeeTime;
+
+    for (let i = 0; i < 8; i++) {
+        wakeTimeDate = new Date(wakeTimeDate - 5400000);
+        recommendTime.push(wakeTimeDate.toTimeString().slice(0, 5));
+        recommandTimeSpan[i].innerHTML = recommendTime[i];
+    }
+
+    modalInstances[1].open();
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+    let timeElems = document.querySelectorAll('.timepicker');
+    let timeInstances = M.Timepicker.init(timeElems, timePickerOpt);
+    
+    let modalElems = document.querySelectorAll('.modal');
+    modalInstances = M.Modal.init(modalElems);
+});
